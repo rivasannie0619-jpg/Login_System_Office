77009090c5650e9ea30f5ae97a7cf089de71a517
unknown <rivasannie0619@gmail.com>
2026-07-14 22:04:34 +0800

    dashboard.blade.php

--- myFirstproject/resources/views/dashboard.blade.php
+++ myFirstproject/resources/views/dashboard.blade.php
@@ -30,12 +30,6 @@
                 </div>
                 
             </div>
-
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
         </div>
     </div>
 </x-app-layout>
